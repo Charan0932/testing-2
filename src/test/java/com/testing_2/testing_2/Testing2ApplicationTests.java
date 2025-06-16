dacb8e1155558303cc392051c5922a001f282cf6
charanbhajantri@gmail.com <charan>
2025-06-16 11:35:47 +0530

    First Commit

---
+++ src/test/java/com/testing_2/testing_2/Testing2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing_2.testing_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testing2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
